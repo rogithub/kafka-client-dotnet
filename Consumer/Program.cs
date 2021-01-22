fd5f6e0201d8629be0d213ca56b0bc81c4a9a21b
rodrigo debian <correo.rodrigo@gmail.com>
2021-01-22 14:26:05 -0600

    examples

---
+++ Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "text-consumer-group1",
+                BootstrapServers = "localhost:29092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(config).Build();
+            c.Subscribe("test");
+
+            // Because Consume is a blocking call, 
+            // we want to capture Ctrl+C and use a cancellation token 
+            // to get out of our while loop and close the consumer gracefully.
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    // Consume a message from the test topic. 
+                    // Pass in a cancellation token so we can break out of our loop when Ctrl+C is pressed
+                    var cr = c.Consume(cts.Token);
+                    Console.WriteLine($"Consumed message '{cr.Message.Value}' from topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}");
+
+                    // Do something interesting with the message you consumed
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
+
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Exercise
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:29092"
+            };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            for (var i = 0; i < 3; i++)
+            {
+                var msg = new Message<Null, string>
+                {
+                    Value = $"Message #{i}"
+                };
+
+                var dr = await p.ProduceAsync("test", msg);
+                Console.WriteLine($"Produced '{dr.Value}' to topic {dr.Topic}, partition {dr.Partition}, offset {dr.Offset}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
